cd0300eb324073f7d86bb5d8b14ef20c87093d1f
govind <gs5818475>
2021-07-05 10:36:30 +0530

    f

--- src/utils/Logo.js
+++ src/utils/Logo.js
@@ -4,7 +4,6 @@ import React from 'react';
 const Logo = () => {
   return(
     <div data-aos="fade-in"className="logo">
-      <h1>DSCVR</h1>
       <h6 className="desclogo">A webapp that analyzes your favorite music and helps you discover new tracks you'll love.</h6>
     </div>
   )
