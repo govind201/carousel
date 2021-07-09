e4a3ece8ca2f375acc94f3b64e30edb605019d62
govind <gs5818475>
2021-07-09 14:53:14 +0530

    made title pretty

--- src/components/modules/Title.js
+++ src/components/modules/Title.js
@@ -4,7 +4,18 @@ import './Title.css'
 export default function Title() {
     return (
         <div className = "title">
-            Carousel
+             <div class="carousel">
+            <p>
+                <span class="c">c</span>
+                <span class="a">a</span>
+                <span class="r">r</span>
+                <span class="o">o</span>
+                <span class="u">u</span>
+                <span class="s">s</span>   
+                <span class="e">e</span>   
+                <span class="l">l</span>   
+            </p>
+        </div>
         </div>
     )
 }
