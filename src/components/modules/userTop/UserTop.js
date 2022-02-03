9fc4162089cbd3b79322f056ed7202cf1a87689e
Hassan Suhail <hassansuhail322@gmail.com>
2022-02-03 14:24:57 +0530

    listdesign

--- src/components/modules/userTop/UserTop.js
+++ src/components/modules/userTop/UserTop.js
@@ -1,32 +1,141 @@
-import React from 'react';
-import Login from '../../pages/Login';
+import React from "react";
+import Login from "../../pages/Login";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Fade from 'react-reveal/Fade';
+import { makeStyles } from '@mui/styles';
 
-const UserTop = ({ token, topArtists, topTracks}) => {
 
 
-  
- console.log("topArtists array in UserTop.js", topArtists, "topTracks in userTop", topTracks)
+const useStyles = makeStyles({
+  root: {
+    background: 'rgb(238,174,202)',
+    border: 0,
+    borderRadius: 3,
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+    
+  },
+});
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+
+
+
+
+const UserTop = ({ token, topArtists, topTracks }) => {
+  console.log(
+    "topArtists array in UserTop.js",
+    topArtists,
+    "topTracks in userTop",
+    topTracks
+  );
+  const classes = useStyles();
+
   return (
-    <div>
-      {!token  &&   <Login  from = "usertop.js" />   }
-      <p> user top Tracks</p>
-      <ol>
-        {topArtists.map((item) => (
-          <li key={item.uri}>
-            <div>{item.name}</div>
-          </li>
-        ))}
-      </ol>
 
-      <ol>
-        { topTracks && topTracks.map((item) => (
-          <li key={item.id}>
-            <div>{item.name}</div>
-          </li>
-        ))}
-      </ol>
-    </div>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <Item>
+            <Fade left  big>
+            <Typography variant="h4" style ={{fontWeight: 600}}className={classes.root}>Top Artists</Typography>
+            </Fade>
+              {!token && <Login from="usertop.js" />}
+              {topArtists.map((item) => (
+                <List
+                  sx={{
+                    width: "100%",
+                    maxWidth: 360,
+                    bgcolor: "background.paper",
+                  }}
+                >
+                  <Divider variant="inset" component="li" />
+                  <ListItem alignItems="flex-start" key={item.uri}>
+                    <ListItemAvatar>
+                      <Avatar alt="Cindy Baker" src={item.images[0].url} />
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary={item.name}
+                      secondary={
+                        <React.Fragment>
+                          <Typography
+                            sx={{ display: "inline" }}
+                            component="span"
+                            variant="body2"
+                            color="text.primary"
+                          >
+                            {item.genres[0]}
+                          </Typography>
+                        </React.Fragment>
+                      }
+                    />
+                  </ListItem>
+                </List>
+              ))}
+            </Item>
+          </Grid>
+          <Grid item xs={6}>
+            <Item>
+            <Fade right big>
+
+            <Typography variant="h4" style ={{fontWeight: 600}}className={classes.root}>Top Tracks</Typography>
+            </Fade>
+              {topTracks &&
+                topTracks.map((item) => (
+                  <List
+                    sx={{
+                      width: "100%",
+                      maxWidth: 360,
+                      bgcolor: "background.paper",
+                    }}
+                  >
+                      <Divider variant="inset" component="li" />
+
+                    <ListItem alignItems="flex-start" key={item.id}>
+                      <MusicNoteIcon fontSize="large" color="primary" />
+                      <ListItemText
+                        primary={item.name}
+                        secondary={
+                          <React.Fragment>
+                            <Typography
+                              sx={{ display: "inline" }}
+                              component="span"
+                              variant="body2"
+                              color="text.primary"
+                            >
+                              {" "}
+                              By - {item.artists[0].name}
+                            </Typography>
+                          </React.Fragment>
+                        }
+                      />
+                    </ListItem>
+                  </List>
+                ))}
+            </Item>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
   );
 };
 
-export default UserTop; 
+export default UserTop;
